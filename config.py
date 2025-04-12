16157d64c8c96a9085404d23f8477813cf2d8088
Guilherme Jacyzin <jacyzin@gmail.com>
2025-04-12 02:14:41 -0300

    Initial commit project - Curso de Engenharia de Machine Learning - Projeto guiado para classificação de texto com posts do Reddit

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-client_id = "z2VyP1nWszAf74_OHewSAA"
-client_secret = "1kd1yWPLeP6ECBFTlx72tR0OShRArA"
-password = "h/ru!8#?W4qrG3B"
-user_agent = "mbraindev app"
-username = "Ok_Ship180"
